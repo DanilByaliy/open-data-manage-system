e3657b205ecb603718204066540429d8ea8f7d05
DanilByaliy <byaliy03@gmail.com>
2021-12-16 16:44:12 +0200

    add sequelize models and associate function

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+const {
+    Action,
+    ActionType,
+    AvailableAction,
+    AvailableFor,
+    Category,
+    DataFile,
+    Dataset,
+    Grant,
+    MetadataKey,
+    MetadataValue,
+    Role,
+    State,
+    Type,
+    User,
+  } = require('./models/model');
+  
+  (async () => {
+    const users = await User.findAll();
+    console.log('All users:', JSON.stringify(users, null, 4));
+  
+    const dasha = User.create({ name: 'Dasha', password: '123parol' });
+  })();
